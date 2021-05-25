53e0c9d0d466b73e2cb34b05439aa91b966f694a
Agusti Parmisano Sabbione <agustinparmisano@gmail.com>
2021-05-25 19:31:12 -0300

    subiendo duelo andando con seleccion de habilidades pero solo hay una de aumento danio, hay que hacer un strategy, se suma un archivo todos

---
+++ test/TODOs.md
@@ -0,0 +1,9 @@
+# TODOS.md
+
+
+## Logica Juego
+
+### Habilidades
+- Hay que pensar un patron strategy para cada habilidad o efecto con una funcion aplicar efectos para que se tenga que llamar a esa funcion con los 2 pkm y se
+resuelva internamente segun el efecto en vez de hacer una funcion con muchos ifs segun el efecto
+
--- test/pokemon1.py
+++ test/pokemon1.py
@@ -1,5 +1,6 @@
 import random
 import time
+import ast
 
 class Pokemon:
 
@@ -24,7 +25,7 @@ class Habilidad:
         for efecto in self.efectos:
             descripcion_efectos += str(efecto)
         
-        return "Habilidad {} con coste {}, probabilidad {} y efectos {}".format(self.nombre, self.costes, self.probabilidad, descripcion_efectos) 
+        return "{} con coste {}, probabilidad {} y efectos {}".format(self.nombre, self.costes, self.probabilidad, descripcion_efectos) 
 
 class EfectoDanio:
 
@@ -39,6 +40,18 @@ class EfectoAumentoDanio(EfectoDanio):
     def __init__(self, danio, probabilidad):
         self.danio = danio
         self.probabilidad = probabilidad
+
+    def __str__(self):
+        return str(self.__dict__)
+
+
+class EfectoDisminucionDanio(EfectoDanio):
+
+    def __init__(self, dimuninucion_danio, probabilidad):
+        self.dimuninucion_danio = dimuninucion_danio
+        self.probabilidad = probabilidad
+        self.accion = "disminucion"
+        self.objetivo = "enemigo"
 
     def __str__(self):
         return str(self.__dict__)
@@ -58,7 +71,6 @@ prob_golpe_fuego = 0
 ed_golpe_fuego = [EfectoDanio(30)]
 habilidad_golpe_fuego = Habilidad("golpe fuego",coste_golpe_fuego,prob_golpe_fuego, "un golpe de fuego",ed_golpe_fuego)
 
-
 coste_llamarada = ["fuego"]
 prob_llamarada = [1,2,3,4]
 ed_llamarada = [EfectoDanio(10), EfectoAumentoDanio(40,[2,3,4])]
@@ -68,27 +80,37 @@ habilidades_fuego = [habilidad_golpe_fuego, habilidad_llamarada]
 poke1 = Pokemon("poke_fuego", "fuego", 50, habilidades_fuego)
 
 energia_agua = Energia("agua","agua")
-coste_h2 = ["agua"]
-prob_h2 = 0
-ed2 = [EfectoDanio(20)]
-h2 = Habilidad("golpe agua",coste_h2,prob_h2, "desc2",ed2)
-habilidades_agua = [h2]
+
+coste_golpe_agua = ["agua"]
+prob_golpe_agua = 0
+ed_golpe_agua = [EfectoDanio(20)]
+golpe_agua = Habilidad("golpe agua",coste_h2,prob_h2, "desc2",ed2)
+
+coste_burbujas = ["agua"]
+prob_burbujas = [1,2,3]
+ed_burbujas = [EfectoDanio(30), EfectoDisminucionDanio(30,[1,2,3,4])]
+burbujas = Habilidad("burbujas",coste_burbujas,prob_burbujas, "desc2",ed_burbujas)
+
+habilidades_agua = [golpe_agua,burbujas]
 poke2 = Pokemon("poke_agua", "agua", 50, habilidades_agua)
 
 
 def seleccion_habilidad(poke):
-    habilidades_disponibles = [str(habilidad) for habilidad in poke.habilidades]            
-    my_dict1 = dict(zip(habilidades_disponibles,range(len(habilidades_disponibles))))
-    habilidades_seleccion = {y:x for x,y in my_dict1.items()}
+    habilidades_disponibles_imprimir = [str(habilidad) for habilidad in poke.habilidades]            
+    habilidades_disponibles = [habilidad for habilidad in poke.habilidades]            
+    dict_habilidades_disponibles_imprimir = dict(zip(habilidades_disponibles_imprimir,range(len(habilidades_disponibles_imprimir))))
+    dict_habilidades_disponibles = dict(zip(habilidades_disponibles,range(len(habilidades_disponibles))))
+    habilidades_seleccion_imprimir = {y:x for x,y in dict_habilidades_disponibles_imprimir.items()}
+    habilidades_seleccion = {y:x for x,y in dict_habilidades_disponibles.items()}
     
     ok = False
     while not ok:
-
-        print(habilidades_seleccion)
+        print("Habilidades disponibles")
+        print(habilidades_seleccion_imprimir)
         habilidad_numero_seleccion=int(input('Seleccione una habilidad:'))
         if habilidad_numero_seleccion in list(habilidades_seleccion.keys()):
             habilidades_seleccionada=habilidades_seleccion[habilidad_numero_seleccion]
-            print("Habilidad seleccionada: ".format(habilidades_seleccionada))
+            print("Habilidad seleccionada: {}".format(str(habilidades_seleccionada)))
             ok = True
             return habilidades_seleccionada
         else:
@@ -96,6 +118,7 @@ def seleccion_habilidad(poke):
             
 
 def aplicar_efectos(habilidad):
+    efectos = ""
     if habilidad.probabilidad != 0:
         dado = random.randint(1,6)
         print("El dado salio con {}".format(dado))
@@ -109,8 +132,10 @@ def aplicar_efectos(habilidad):
                     time.sleep(2)
                     if dado in efecto.probabilidad:
                         print("Se va a aplicar {}".format(efecto))
+                        efectos = str(efecto)
                 else:
                     print("Se va a aplicar {}".format(efecto))
+                    efectos = str(efecto)
     else:
         for efecto in habilidad.efectos:
             if hasattr(efecto, 'probabilidad'):
@@ -119,8 +144,12 @@ def aplicar_efectos(habilidad):
                 time.sleep(2)
                 if dado in efecto.probabilidad:
                         print("Se va a aplicar {}".format(efecto))
+                        efectos = str(efecto)
             else:
                 print("Se va a aplicar {}".format(efecto))
+                efectos = str(efecto)
+
+    return efectos
 
 
 def duelo(poke1, poke2):
@@ -140,48 +169,28 @@ def duelo(poke1, poke2):
                         
             efecto=aplicar_efectos(habilidad_seleccionada)
             time.sleep(2)
-            print("EFECTO FINAL: ".format(efecto))
-            
-            '''
-            if dado in poke1.habilidades[1].probabilidad:
-                danio = poke1.habilidades[1].efectos[0].danio
-                print("{} lanza {} por {} de danio".format(poke1.nombre, poke1.habilidades[1].nombre, danio))
-                time.sleep(2)
-                
-                if hasattr(poke1.habilidades[1], 'probabilidad'):
-                    dado = random.randint(1,6)
-                    print("El dado salio con {}".format(dado))
-                    time.sleep(2)
-                    
-                    if dado in poke1.habilidades[1].probabilidad:
-                        danio = poke1.habilidades[1].efectos[1].danio
-                        print("{} lanza {} por {} de danio".format(poke1.nombre, poke1.habilidades[1].nombre, danio))
-                        time.sleep(2)
+            print("EFECTO FINAL: {}".format(str(efecto)))
 
-
-                poke2.vida = poke2.vida - danio
-                print("{} es dañado con {} y le queda {} de vida".format(poke2.nombre, danio, poke2.vida))
-                time.sleep(2)
-
+            poke2.vida = poke2.vida - int(ast.literal_eval(efecto)["danio"])
+            print("{} queda con {} de vida".format(poke2.nombre, poke2.vida))
 
         if poke2.vida > 0:
-            
             print("Turno de {}".format(poke2.nombre))
             time.sleep(2)
-            dado = random.randint(1,6)    
-            print("El dado salio con {}".format(dado))
+
+            if turno == 1:
+                habilidad_seleccionada=poke2.habilidades[0]
+                print("Es el turno {} de {} la habilidad es {}".format(turno, poke2.nombre, habilidad_seleccionada))
+            else:
+                habilidad_seleccionada=seleccion_habilidad(poke2)
+                        
+            efecto=aplicar_efectos(habilidad_seleccionada)
             time.sleep(2)
+            print("EFECTO FINAL: {}".format(str(efecto)))
 
-            if dado in poke2.habilidades[0].probabilidad:
-                danio = poke2.habilidades[0].efectos[0].danio
-
-                print("{} lanza {} por {} de danio".format(poke2.nombre, poke2.habilidades[0].nombre, danio))
-                time.sleep(2)
-
-                poke1.vida = poke1.vida - danio
-                print("{} es dañado con {} y le queda {} de vida".format(poke1.nombre, danio, poke1.vida))
-                time.sleep(2)
-        '''
+            poke1.vida = poke1.vida - int(ast.literal_eval(efecto)["danio"])
+            print("{} queda con {} de vida".format(poke1.nombre, poke1.vida))
+            
         turno+=1
 
     if poke1.vida > 0:
